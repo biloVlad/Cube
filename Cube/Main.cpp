6b5c06a2f9fac1ebe10916d1f8a248fa1971e6d7
IATOMI <IATOMI@PREDATOR40000>
2017-06-17 12:02:58 +0300

    Добавьте файлы проекта.

---
+++ Cube/Main.cpp
@@ -0,0 +1,7 @@
+#include "Main.h"
+
+using namespace Cube;
+
+int main(array<String^> ^args) {
+	Application::Run(gcnew Main());
+}
---
+++ Cube/Main.h
@@ -0,0 +1,65 @@
+#pragma once
+
+namespace Cube {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ Main
+	/// </summary>
+	public ref class Main : public System::Windows::Forms::Form
+	{
+	public:
+		Main(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~Main()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->SuspendLayout();
+			// 
+			// Main
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(632, 253);
+			this->Name = L"Main";
+			this->Text = L"Main";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	};
+}
