96bd17d4b5588df6fd05823856afac11a56fe520
IATOMI <IATOMI@PREDATOR40000>
2017-06-21 15:31:07 +0300

    Add sound for hover mod

--- Cube/Main.h
+++ Cube/Main.h
@@ -67,8 +67,7 @@ namespace Cube {
 			this->logo_f2l->Size = System::Drawing::Size(315, 239);
 			this->logo_f2l->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
 			this->logo_f2l->TabIndex = 0;
-			this->logo_f2l->TabStop = false;
-			this->logo_f2l->Click += gcnew System::EventHandler(this, &Main::pictureBox1_Click);
+			this->logo_f2l->TabStop = false;			
 			this->logo_f2l->MouseHover += gcnew System::EventHandler(this, &Main::logo_f2l_MouseHover);
 			this->logo_f2l->MouseLeave += gcnew System::EventHandler(this, &Main::logo_f2l_MouseLeave);
 
@@ -98,18 +97,9 @@ namespace Cube {
 			this->PerformLayout();
 
 		}
-#pragma endregion
-	private: System::Void pictureBox1_Click(System::Object^  sender, System::EventArgs^  e) {
-	}
-	private: System::Void logo_f2l_MouseHover(System::Object^  sender, System::EventArgs^  e) {
-		this->logo_f2l->Location = System::Drawing::Point(2, 68);
-		this->logo_f2l->Size = System::Drawing::Size(365, 289);
-		
-		
-	}
-	private: System::Void logo_f2l_MouseLeave(System::Object^  sender, System::EventArgs^  e) {
-		this->logo_f2l->Location = System::Drawing::Point(12, 78);
-		this->logo_f2l->Size = System::Drawing::Size(315, 239);
-	}
+#pragma endregion	
+	private: System::Void logo_f2l_MouseHover(System::Object^  sender, System::EventArgs^  e);
+	private: System::Void logo_f2l_MouseLeave(System::Object^  sender, System::EventArgs^  e);
+	
 	};
 }
